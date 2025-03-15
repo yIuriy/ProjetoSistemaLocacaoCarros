f07b9ad6b49f18c5ad8688bb654e9832f2df4505
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-15 09:31:29 -0300

    modify name

--- src/com/iuri/services/CheckPlateStandard.java
+++ src/com/iuri/services/CheckPlateStandard.java
@@ -2,7 +2,7 @@ package com.iuri.services;
 
 import java.util.regex.Pattern;
 
-public class CheckPlateStandart {
+public class CheckPlateStandard {
     public static boolean check(String plate){
         String regex = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$";
         return Pattern.matches(regex, plate);
