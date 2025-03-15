3365255ad94f4f04b323b53aecdc3fe5dd4c08a4
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-15 08:44:16 -0300

    Update Vehicle.java

--- src/com/iuri/domain/Vehicle.java
+++ src/com/iuri/domain/Vehicle.java
@@ -1,6 +1,6 @@
 package com.iuri.domain;
 
-public abstract class Vehicle {
+public abstract class Vehicle implements Rentable{
     protected String name;
     protected String model;
     protected String plate;
@@ -12,5 +12,25 @@ public abstract class Vehicle {
         this.model = model;
         this.plate = plate;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getRentPricePerDay() {
+        return rentPricePerDay;
     }
 }
