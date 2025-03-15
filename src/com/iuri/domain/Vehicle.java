ca9654374a2944c6e2df85d5fc3804d5219c8dac
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-15 07:48:17 -0300

    create abstract class Vehicle
    added basics attributes

---
+++ src/com/iuri/domain/Vehicle.java
@@ -0,0 +1,13 @@
+package com.iuri.domain;
+
+public abstract class Vehicle {
+    protected String name;
+    protected String model;
+    protected String plate;
+    protected double price;
+    protected double rentPricePerDay;
+
+
+
+
+}
