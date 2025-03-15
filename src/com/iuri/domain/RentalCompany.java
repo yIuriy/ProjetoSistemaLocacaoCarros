78db579fe226f6a582d21db347bd65614e620a60
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-15 10:13:08 -0300

    create RentalCompany

---
+++ src/com/iuri/domain/RentalCompany.java
@@ -0,0 +1,25 @@
+package com.iuri.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentalCompany {
+    private String name;
+    protected List<Customer> customerList = new ArrayList<>();
+    protected List<Vehicle> vehicleList = new ArrayList<>();
+
+    public void addCustomer(Customer customer){
+        customerList.add(customer);
+    }
+    public void addVehicle(Vehicle vehicle){
+        vehicleList.add(vehicle);
+    }
+
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public List<Vehicle> getVehicleList() {
+        return vehicleList;
+    }
+}
