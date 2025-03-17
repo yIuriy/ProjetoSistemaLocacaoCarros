f7013129ed4967c2f4ad17ecc25eab6a5e617205
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-17 09:58:20 -0300

    change the logic between the find

--- src/com/iuri/services/ReturnVehicle.java
+++ src/com/iuri/services/ReturnVehicle.java
@@ -5,13 +5,13 @@ import com.iuri.domain.RentalCompany;
 import com.iuri.domain.Vehicle;
 
 public class ReturnVehicle {
-    public void returnVehicle(Customer customer, RentalCompany company, String vehicleName) {
+    public static void returnVehicle(Customer customer, RentalCompany company, String plate) {
         if (customer.getRentedVehicles().isEmpty()) {
             System.out.println("Cliente " + customer.getName() + "não possui nenhum veículo alugado.");
             return;
         }
         for (Vehicle rentedVehicle : customer.getRentedVehicles()) {
-            if (rentedVehicle.getName().equals(vehicleName)) {
+            if (rentedVehicle.getPlate().equals(plate)) {
                 rentedVehicle.setAvailable(true);
                 customer.removeVehicleFromRentedList(rentedVehicle);
             }
