2f6d42a482e872fc88320dd703909ca99317f922
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-17 09:27:58 -0300

    Update CarType.java

--- src/com/iuri/domain/CarType.java
+++ src/com/iuri/domain/CarType.java
@@ -1,12 +1,9 @@
 package com.iuri.domain;
 
 public enum CarType {
-    SUV(1),
-    PICKUP(2),
-    SEDAN(3),
-    SPORTY(4)
-    ;
-
-    CarType(int i) {
-    }
+    SUV,
+    PICKUP,
+    SEDAN,
+    SPORTY
 }
+
