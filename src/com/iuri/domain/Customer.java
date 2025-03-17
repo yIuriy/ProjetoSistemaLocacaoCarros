fa305a59c9f6657d796f2b54d95fe311e16ff857
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-17 09:28:52 -0300

    added password attribute

--- src/com/iuri/domain/Customer.java
+++ src/com/iuri/domain/Customer.java
@@ -1,7 +1,6 @@
 package com.iuri.domain;
 
 import com.iuri.services.CheckCpfStandard;
-import com.iuri.services.CheckPlateStandard;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +8,12 @@ import java.util.List;
 public class Customer {
     private String name;
     private String cpf;
+    private String password;
     private List<Vehicle> rentedVehicles = new ArrayList<>();
 
-    public Customer(String name, String cpf) {
+    public Customer(String name, String cpf, String password) {
         this.name = name;
+        this.password = password;
         if (CheckCpfStandard.check(cpf)) {
             this.cpf = cpf;
         } else {
@@ -24,15 +25,23 @@ public class Customer {
         return name;
     }
 
-    public void addVehicleToRentedList(Vehicle vehicle){
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void addVehicleToRentedList(Vehicle vehicle) {
         rentedVehicles.add(vehicle);
     }
 
-    public void removeVehicleFromRentedList(Vehicle vehicle){
+    public void removeVehicleFromRentedList(Vehicle vehicle) {
         rentedVehicles.remove(vehicle);
     }
 
     public List<Vehicle> getRentedVehicles() {
         return rentedVehicles;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
