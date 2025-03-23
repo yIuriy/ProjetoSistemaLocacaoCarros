9f8f4227588284b37bab8960d2ef85ce9eab595d
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-03-23 15:47:52 -0300

    Update CreateReport.java

--- src/com/iuri/services/CreateReport.java
+++ src/com/iuri/services/CreateReport.java
@@ -1,9 +1,63 @@
 package com.iuri.services;
 
+import com.iuri.domain.Customer;
 import com.iuri.domain.RentalCompany;
+import com.iuri.domain.Vehicle;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class CreateReport {
-    public void create(RentalCompany company){
-        
+    public static void create(RentalCompany company) {
+        File file = new File("D:\\ProjetoSistemaLocacaoCarros\\reports\\report.txt");
+        try (FileWriter fileWriter = new FileWriter(file, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            LocalDateTime dateNow = LocalDateTime.now();
+            bufferedWriter.write("Relatório " + company.getName() + "[" + dateNow + "]");
+            bufferedWriter.newLine();
+            bufferedWriter.write("Carros alugados:");
+            bufferedWriter.newLine();
+            for (Vehicle vehicle : company.getVehicleList()) {
+                if (!vehicle.isAvailable()) {
+                    bufferedWriter.write("Nome do veículo: " + vehicle.getName() + "| Placa do veículo: " + vehicle.getPlate());
+                    bufferedWriter.newLine();
+                }
+            }
+            bufferedWriter.write("Carros disponíveis para aluguel:");
+            bufferedWriter.newLine();
+            for (Vehicle vehicle : company.getVehicleList()) {
+                if (vehicle.isAvailable()) {
+                    bufferedWriter.write("Nome do veículo: " + vehicle.getName() + "| Placa do veículo: " + vehicle.getPlate());
+                    bufferedWriter.newLine();
+                }
+            }
+            if (!company.getCustomerList().isEmpty()) {
+                bufferedWriter.write("Clientes cadastrados: ");
+                bufferedWriter.newLine();
+                for (Customer customer : company.getCustomerList()) {
+                    bufferedWriter.write("Nome do cliente: ");
+                    bufferedWriter.write(customer.getName());
+                    bufferedWriter.newLine();
+                    if (!customer.getRentedVehicles().isEmpty()) {
+                        {
+                            bufferedWriter.write("Carros alugados por "+ customer.getName() + ": ");
+                            bufferedWriter.newLine();
+                            for (Vehicle rentedVehicle : customer.getRentedVehicles()) {
+                                bufferedWriter.write(rentedVehicle.getName());
+                                bufferedWriter.newLine();
+                            }
+                        }
+                    }
+                }
+            }
+            bufferedWriter.write("===================================================================");
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
